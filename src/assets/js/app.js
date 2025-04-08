db3199b5e31a7f1b03b1e532f1ae808fcc837faa
Amankhare01 <amankhare.aa@gmail.com>
2025-04-08 13:09:15 +0530

    d

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,20 +6,20 @@
  *   Webflow.push(readyFunction);
  */
 //Subscription captcha code start
-setInterval(() => {
-  if (grecaptcha.getResponse().length > 0) {
-      console.log("CAPTCHA verified, enabling submit button.");
-      document.getElementById("submit-button-subs").disabled = false;
-  } else {
-      console.log("CAPTCHA not verified, keeping submit button disabled.");
-      document.getElementById("submit-button-subs").disabled = true;
-  }
-});
+// setInterval(() => {
+//   if (grecaptcha.getResponse().length > 0) {
+//       console.log("CAPTCHA verified, enabling submit button.");
+//       document.getElementById("submit-button-subs").disabled = false;
+//   } else {
+//       console.log("CAPTCHA not verified, keeping submit button disabled.");
+//       document.getElementById("submit-button-subs").disabled = true;
+//   }
+// });
 
-function enableSubmit() {
-  let submitButton = document.getElementById("submit-button-subs");
-  submitButton.disabled = false; // Enable button
-}
+// function enableSubmit() {
+//   let submitButton = document.getElementById("submit-button-subs");
+//   submitButton.disabled = false; // Enable button
+// }
 
 //Subscription code ended here
 document.addEventListener("DOMContentLoaded", function () {
