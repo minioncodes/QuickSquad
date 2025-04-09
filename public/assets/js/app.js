44c674b2cd4d866578b6d6230729f949d83f4568
Amankhare01 <amankhare.aa@gmail.com>
2025-04-09 13:25:23 +0530

    jk

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,27 +1,3 @@
-/*!
- * Webflow: Front-end site library
- * @license MIT
- * Inline scripts may access the api using an async handler:
- *   var Webflow = Webflow || [];
- *   Webflow.push(readyFunction);
- */
-//Subscription captcha code start
-// setInterval(() => {
-//   if (grecaptcha.getResponse().length > 0) {
-//       console.log("CAPTCHA verified, enabling submit button.");
-//       document.getElementById("submit-button-subs").disabled = false;
-//   } else {
-//       console.log("CAPTCHA not verified, keeping submit button disabled.");
-//       document.getElementById("submit-button-subs").disabled = true;
-//   }
-// });
-
-// function enableSubmit() {
-//   let submitButton = document.getElementById("submit-button-subs");
-//   submitButton.disabled = false; // Enable button
-// }
-
-//Subscription code ended here
 document.addEventListener("DOMContentLoaded", function () {
   const searchIcon = document.getElementById("search-icon");
   const searchContainer = document.querySelector(".search-container");
