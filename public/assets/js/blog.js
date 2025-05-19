a404ace38c6852fc9654e8ba4e4cecfdd3cee589
Amankhare01 <amankhare.aa@gmail.com>
2025-05-19 12:01:45 +0530

    image optimization

--- public/assets/js/blog.js
+++ public/assets/js/blog.js
@@ -3,63 +3,63 @@
 const blogs = [
   {
     "title": "How to Fix Slow Internet and Connectivity Issues",
-    "image": "https://procol.co.uk/wp-content/uploads/2017/07/news-internet-frustration-01.jpg",
+    "image": "/assets/1.jpg",
     "date": "February 10, 2025",
     "url": "/blogs/Slow-Internet-Issues",
     "description": "Slow internet can be frustrating! Learn how to troubleshoot your network, optimize router settings, and speed up your connection with simple steps."
   },
   {
     "title": "Common Computer Problems & How to Fix Them",
-    "image": "https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2024/07/a-blue-screen-of-death-error-appearing-on-a-windows-laptop-while-a-woman-is-using-it.jpg",
+    "image": "/assets/2.jpg",
     "date": "February 10, 2025",
     "url": "/Blogs/Computer-Problem",
     "description": "Facing issues like freezing, crashing, or blue screen errors? Here’s a step-by-step guide to fixing common PC problems on your own."
   },
   {
     "title": "Protecting Your Devices from Viruses & Malware",
-    "image": "https://media.istockphoto.com/id/1047952354/vector/alert-message-of-virus-detected-scanning-and-identifying-computer-virus-inside-binary-code.jpg?s=612x612&w=0&k=20&c=a4urCTse4J29kN9wxXqN9efnyWg_c7udz9kvf2RGAyE=",
+    "image": "/assets/3.jpg",
     "date": "February 10, 2025",
     "url": "/Blogs/Protecting-Your-Devices-from-Viruses-&-Malware",
     "description": "Keep your devices safe from cyber threats! Learn about antivirus, malware removal, and best security practices to stay safe."
   },
   {
     "title": "Remote Tech Support: How It Works & When You Need It",
-    "image": "https://unity-connect.com/wp-content/uploads/2023/05/Complete-Guide-to-Remote-Desktop-Support-Software_670276063.jpg",
+    "image": "/assets/4.jpg",
     "date": "February 10, 2025",
     "url": "/Blogs/Remote-Tech-Support",
     "description": "Need tech help but can’t visit a service center? Learn how remote tech support works, its benefits, and when to use it for troubleshooting."
   },
   {
     "title": "Best Tips to Optimize Your PC Performance",
-    "image": "https://www.hp.com/us-en/shop/app/assets/images/uploads/prod/7-ways-to-improve-computer-performance-11549066174778.jpg",
+    "image": "/assets/5.jpg",
     "date": "February 10, 2025",
     "url": "/Blogs/Best-Tips-to-Optimize-Your-PC-Performance",
     "description": "Is your PC running slow? Follow these expert tips to clean up junk files, manage startup programs, and boost system performance easily!"
   },
   {
     "title": "How AI-Powered Chatbots Are Revolutionizing Tech Support in 2025",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/656e010c1eacbfe7918ca848/AI-Customer-Service/960x0.png?format=png&width=960",
+    "image": "/assets/6.jpg",
     "date": "April 2, 2025",
     "url": "/Blogs/AI-Powered",
     "description": "AI-powered chatbots are transforming tech support! discover how businesses can enhance customer support with AI-driven solutions."
   },
   {
     "title": "Millennials vs. Gen Z: How Their Customer Support Expectations Differ",
-    "image": "https://callcenterstudio.com/wp-content/uploads/2023/07/Millennials-vs.-Gen-Z_-What-Type-of-Customer-Experience-is-Gen-Z-Expecting.webp",
+    "image": "/assets/7.jpg",
     "date": "April 8, 2025",
     "url": "/blogs/Tech-support/",
     "description": "Explore how Millennials and Gen Z approach customer support differently, and how your business can adapt to serve both effectively."
   },
   {
     "title": "The Most Frequent Questions We Get at Our Help Desk",
-    "image": "https://styledispatch.com/wp-content/uploads/2021/01/shutterstock_1050638456-660x400.jpg",
+    "image": "/assets/8.jpg",
     "date": "April 14, 2025",
     "url": "/blogs/help-desk-questions/",
     "description": "Discover how our help desk handles incidents, routine changes, user queries, and more—with insights from real support data."
   },
   {
     "title": "How Artificial Intelligence Can Improve IT for Businesses",
-    "image": "https://micropro.com/wp-content/uploads/2022/09/xrobotic-helping-hand.jpg.pagespeed.ic.W2HELOHRXy.webp",
+    "image": "/assets/9.jpg",
     "date": "May 6, 2025",
     "url": "/blogs/ai-in-it-support/",
     "description": "Learn how AI is revolutionizing IT support by reducing costs, enhancing efficiency, and securing networks—tailored for modern business needs."
